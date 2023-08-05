eac9004d31f8491e8a38fbaf3df2056356178c2d
Dhara <dhara041092@gmail.com>
2023-08-05 10:20:30 +0530

    Updates

---
+++ precache-manifest.ab6964d6a922c06ced40106390b05ad6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e5782c179fffc2040b1",
+    "url": "/Textutils/static/css/main.7a9ff666.chunk.css"
+  },
+  {
+    "revision": "0e5782c179fffc2040b1",
+    "url": "/Textutils/static/js/main.0e5782c1.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/Textutils/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/Textutils/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "fa4348f6a4b91f9df22e",
+    "url": "/Textutils/static/js/runtime~main.fa4348f6.js"
+  },
+  {
+    "revision": "248f3acf8cc1224dd5142a52f0c8c6be",
+    "url": "/Textutils/index.html"
+  }
+];
